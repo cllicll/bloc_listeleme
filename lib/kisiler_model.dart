21ca246c571c6d1928c8ab0c9100e8f5c8bb66c9
Celal İçelli <cllicll@hotmail.com>
2022-05-26 11:24:51 +0300

    null control

--- lib/kisiler_model.dart
+++ lib/kisiler_model.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Kisiler {
-  String kisi_id;
-  String kisi_ad;
-  String kisi_tel;
+  String? kisi_id;
+  String? kisi_ad;
+  String? kisi_tel;
 
   Kisiler(this.kisi_id, this.kisi_ad, this.kisi_tel);
 }
